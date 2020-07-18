5a7bc5c3f9b2880b4ff7c6a672766fe13acf0023
Blake Albertson <albertson.blake@yahoo.com>
2020-07-18 06:46:06 -0400

    init

---
+++ include/play.js
@@ -0,0 +1,159 @@
+const ytdlDiscord = require("discord-ytdl-core");
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const scdl = require("soundcloud-downloader");
+const { canModifyQueue } = require("../util/EvobotUtil");
+
+module.exports = {
+  async play(song, message) {
+    const { PRUNING, SOUNDCLOUD_CLIENT_ID } = require("../config.json");
+    const queue = message.client.queue.get(message.guild.id);
+
+    if (!song) {
+      queue.channel.leave();
+      message.client.queue.delete(message.guild.id);
+      return queue.textChannel.send("🚫 Music queue ended.").catch(console.error);
+    }
+
+    let stream = null;
+
+    try {
+      if (song.url.includes("youtube.com")) {
+        //stream = await ytdlDiscord(song.url, {quality: [128, 127, 120, 96, 95, 94, 93], highWaterMark: 1 << 25 });
+        // Edit: the code below is now my "workaround" until the issue is fixed.
+
+        stream = ytdl(song.url, {
+          quality: [128, 127, 120, 96, 95, 94, 93],
+          opusEncoded: false,
+          fmt: "mp3",
+          encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+        });
+
+        
+      } else if (song.url.includes("soundcloud.com") && SOUNDCLOUD_CLIENT_ID) {
+        const info = await scdl.getInfo(song.url, SOUNDCLOUD_CLIENT_ID);
+        const opus = scdl.filterMedia(info.media.transcodings, { format: scdl.FORMATS.OPUS });
+        stream = await scdl.downloadFromURL(opus[0].url, SOUNDCLOUD_CLIENT_ID);
+      } else if (song.url.includes("mp3" || "m4a")) {
+
+        stream = song.url;
+
+      }
+    } catch (error) {
+      if (queue) {
+        queue.songs.shift();
+        module.exports.play(queue.songs[0], message);
+      }
+
+      console.error(error);
+      return message.channel.send(`Error: ${error.message ? error.message : error}`);
+    }
+
+    queue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id));
+
+    //const type = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
+
+    //console.log(type);
+
+    const dispatcher = queue.connection
+      .play(stream, { type: "unknown" })
+      .on("finish", () => {
+        if (collector && !collector.ended) collector.stop();
+
+        if (queue.loop) {
+          // if loop is on, push the song back at the end of the queue
+          // so it can repeat endlessly
+          let lastSong = queue.songs.shift();
+          queue.songs.push(lastSong);
+          module.exports.play(queue.songs[0], message);
+        } else {
+          // Recursively play the next song
+          queue.songs.shift();
+          module.exports.play(queue.songs[0], message);
+        }
+      })
+      .on("error", (err) => {
+        console.error(err);
+        queue.songs.shift();
+        module.exports.play(queue.songs[0], message);
+      });
+    dispatcher.setVolumeLogarithmic(queue.volume / 100);
+
+    try {
+      var playingMessage = await queue.textChannel.send(`🎶 Started playing: **${song.title}** ${song.url}`);
+      await playingMessage.react("⏭");
+      await playingMessage.react("⏯");
+      await playingMessage.react("🔁");
+      await playingMessage.react("⏹");
+    } catch (error) {
+      console.error(error);
+    }
+
+    const filter = (reaction, user) => user.id !== message.client.user.id;
+    var collector = playingMessage.createReactionCollector(filter, {
+      time: song.duration > 0 ? song.duration * 1000 : 600000
+    });
+
+    collector.on("collect", (reaction, user) => {
+      if (!queue) return;
+      const member = message.guild.member(user);
+
+      switch (reaction.emoji.name) {
+        case "⏭":
+          queue.playing = true;
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return;
+          queue.connection.dispatcher.end();
+          queue.textChannel.send(`${user} ⏩ skipped the song`).catch(console.error);
+          collector.stop();
+          break;
+
+        case "⏯":
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return;
+          if (queue.playing) {
+            queue.playing = !queue.playing;
+            queue.connection.dispatcher.pause(true);
+            queue.textChannel.send(`${user} ⏸ paused the music.`).catch(console.error);
+          } else {
+            queue.playing = !queue.playing;
+            queue.connection.dispatcher.resume();
+            queue.textChannel.send(`${user} ▶ resumed the music!`).catch(console.error);
+          }
+          break;
+
+        case "🔁":
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return;
+          queue.loop = !queue.loop;
+          queue.textChannel.send(`Loop is now ${queue.loop ? "**on**" : "**off**"}`).catch(console.error);
+          break;
+
+        case "⏹":
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return;
+          queue.songs = [];
+          queue.textChannel.send(`${user} ⏹ stopped the music!`).catch(console.error);
+          try {
+            queue.connection.dispatcher.end();
+          } catch (error) {
+            console.error(error);
+            queue.connection.disconnect();
+          }
+          collector.stop();
+          break;
+
+        default:
+          reaction.users.remove(user).catch(console.error);
+          break;
+      }
+    });
+
+    collector.on("end", () => {
+      playingMessage.reactions.removeAll().catch(console.error);
+      if (PRUNING && playingMessage && !playingMessage.deleted) {
+        playingMessage.delete({ timeout: 3000 }).catch(console.error);
+      }
+    });
+  }
+};
